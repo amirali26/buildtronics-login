d6e7e1ef5b06bd99a6d6faa33df74dae5b9eaf7f
Amir Shojae <amiralishojaee123@gmail.com>
2021-03-13 23:12:01 +0000

    initial commit added cdk to the project

---
+++ bin/buildtronics-login.ts
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from '@aws-cdk/core';
+import { BuildtronicsLoginStack } from '../lib/buildtronics-login-stack';
+
+const app = new cdk.App();
+new BuildtronicsLoginStack(app, 'BuildtronicsLoginStack');
---
+++ test/buildtronics-login.test.ts
@@ -0,0 +1,13 @@
+import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
+import * as cdk from '@aws-cdk/core';
+import * as BuildtronicsLogin from '../lib/buildtronics-login-stack';
+
+test('Empty Stack', () => {
+    const app = new cdk.App();
+    // WHEN
+    const stack = new BuildtronicsLogin.BuildtronicsLoginStack(app, 'MyTestStack');
+    // THEN
+    expectCDK(stack).to(matchTemplate({
+      "Resources": {}
+    }, MatchStyle.EXACT))
+});
