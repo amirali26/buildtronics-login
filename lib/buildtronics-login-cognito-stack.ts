2da8ba9d3da8274ed8edb64abd2a3c994cde3181
Amir Shojae <amiralishojaee123@gmail.com>
2021-03-14 14:17:44 +0000

    fixed build

--- lib/buildtronics-login-cognito-stack.ts
+++ lib/buildtronics-login-cognito-stack.ts
@@ -36,10 +36,6 @@ export class BuildtronicsLoginStack extends cdk.Stack {
           required: true,
           mutable: false,
         },
-        email: {
-          required: true,
-          mutable: true,
-        },
       },
       mfa: cognito.Mfa.OFF,
       passwordPolicy: {
